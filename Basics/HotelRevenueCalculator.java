48e34f54a5c536ea800d492e90f7d8979af253f9
Joosetha D <joosedhivya2006@gmail.com>
2026-02-15 22:06:42 +0530

    Add files via upload

---
+++ Basics/HotelRevenueCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class HotelRevenueCalculator{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int rooms=sc.nextInt();
+int price=sc.nextInt();
+int serviceCharges=sc.nextInt();
+int commission=sc.nextInt();
+int totalRevenue=(rooms*price)+serviceCharges-commission;
+System.out.println("Total Revenue = "+totalRevenue);
+sc.close();
+}
+}
