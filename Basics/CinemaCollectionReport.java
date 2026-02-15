1c26392a1176e527c664c804bc8052e10cc4c2d3
Joosetha D <joosedhivya2006@gmail.com>
2026-02-15 22:00:52 +0530

    Create CinemaCollectionReport.java

---
+++ Basics/CinemaCollectionReport.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CinemaCollectionReport{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int ticketRevenue=sc.nextInt();
+int snackRevenue=sc.nextInt();
+int maintenance=sc.nextInt();
+int electricity=sc.nextInt();
+int netProfit=ticketRevenue+snackRevenue-maintenance-electricity;
+System.out.println("Net Profit = "+netProfit);
+sc.close();
+}
+}
