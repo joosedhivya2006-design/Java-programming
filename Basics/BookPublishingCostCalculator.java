b8971058b81bfbc62e61ff6a625e6948a1db4742
Joosetha D <joosedhivya2006@gmail.com>
2026-02-16 09:13:19 +0530

    Add files via upload

---
+++ Basics/BookPublishingCostCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BookPublishingCostCalculator{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int copies=sc.nextInt();
+int cost=sc.nextInt();
+int editing=sc.nextInt();
+int design=sc.nextInt();
+int commission=sc.nextInt();
+int marketing=sc.nextInt();
+int profit=(copies*cost)+editing+design-commission-marketing;
+System.out.println("Publishing Profit = "+profit);
+sc.close();
+}
+}
