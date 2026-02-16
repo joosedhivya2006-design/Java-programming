cf1764cae20ad969632de9a8cfb50c8b693d638c
Joosetha D <joosedhivya2006@gmail.com>
2026-02-16 13:54:28 +0530

    Add files via upload

--- Operators/BoxCapcityDoubling.java
+++ Operators/BoxCapcityDoubling.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-import java.math.*;
 public class BoxCapcityDoubling {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int baseBoxes=sc.nextInt();
         int levels=sc.nextInt();
-        double capacity=baseBoxes*(Math.pow(2, levels));
-        System.out.printf("%d",(int)capacity);
+        int capacity=baseBoxes<<levels;
+        System.out.println(capacity);
         sc.close();
     }
 }
